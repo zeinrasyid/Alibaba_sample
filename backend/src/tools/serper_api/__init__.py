767db35ae49ec691af0b087d0a6f57367879268b
zeinrasyid <zeinrasyid18@gmail.com>
2026-03-12 13:24:26 +0700

    up

--- backend/src/tools/serper_api/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.tools.serper_api.serper_google_search import serper_google_search
-from src.tools.serper_api.serper_web_scrap import serper_web_scrap
-
-__all__ = ['serper_google_search', 'serper_web_scrap']
